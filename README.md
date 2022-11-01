c70619fd336ff2b9755ffeef98b3745285aa89dd
aniouar <aniouar@42>
2022-11-01 12:12:38 -0400

    generating range subnet

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-fill all masks possible (Solved not optimized)
-    [*] fill first range 
-    [-] duplicated mask (Solved)
+fill all masks possible (Solved not optimized) <br>
+    [*] fill first range <br>
+    [-] duplicated mask (Solved) <br>
+
+generate a random mask which will generate the problem of a form ip address <br>
+    - find : <br>
+        the range of subnet <br>
+        ip network <br>
+        broadcast <br>
+        first ip <br>
+        last ip <br>
+
+first step (generate mask) (done) <br>
+
+next step based on the mask lets generate a random ip <br>
+    in network portion things seems easy <br>
+    but in host portion we need to define a range and reserve the network ip and broadcast ip <br>
+
+    lets generate the partitions of the subnet mask that we could work in them <br>
+
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import random
+
 
 def add_range(masks,ranges):
     mask = ""
@@ -8,11 +10,8 @@ def add_range(masks,ranges):
     mask = mask.rstrip(".")
     ranges.append(mask)
 
-def main():
-    ranges = []
-    bit = numberOctet = 0
-    masks = [0,0,0,0]
 
+def fill_range(ranges,masks):
     i = 0
     while i < 4:
         if i > 0:
@@ -26,11 +25,65 @@ def main():
             add_range(masks,ranges)
             x -= 1
         masks[i] += 1
-        #add_range(masks,ranges)
         i += 1
     add_range(masks,ranges)
 
-    for range in ranges:
-        print (range)
+def get_rand_range(ranges):
+    lenRanges = len(ranges)
+    index = random.randint(0,lenRanges)
+    range = ranges[index]
+    return (range)
+
+def get_rand_oct_mask(octet_mask):
+    #128 (0,127) (129,255)
+    ranges = []
+    range_size = 256 - octet_mask
+    rounds = int(256 / range_size)
+    start = 0
+    end   = 0
+    i = 0
+    while (i < rounds):
+        if i > 0:
+            start = end + 1
+        range = []
+        end += range_size
+        if(i > 0):
+            range.append(start-1)
+        else:
+            range.append(start)
+        range.append(end-1)
+        ranges.append(range)
+        i += 1
+    print (ranges)
+    return(str(range_size) + " " + str(rounds))
+
+
+def get_rand_ip(range):
+    octets_mask = range.split(".")
+    ip = ""
+    for octet in  octets_mask:
+        if octet == "255":
+            octet_ip = random.randint(1,255)
+            ip += str(octet_ip) + "."
+        else:
+            ip += str(0) + "."
+    ip.rstrip(".")
+    return (ip)
+
+def main():
+    ranges = []
+    bit = numberOctet = 0
+    masks = [0,0,0,0]
+    fill_range(ranges,masks)
+    range = get_rand_range(ranges)
+    ip = get_rand_ip(range)
+    example = get_rand_oct_mask(192)
+    print(example)
+    #print(range + " " + ip)
+
+    
+
+    #for range in ranges:
+        #print (range)
 
 main()
